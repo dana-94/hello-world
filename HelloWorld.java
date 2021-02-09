11e07b52462b1b21e88f3adc1adf19642764c870
dana <dalsiba0@email.cpcc.edu>
2021-02-09 11:56:51 -0500

    Revert "ubdate HelloWorld.java"
    This reverts commit b0343af647c6f9b79cde6030b91110d8f4ecdb24.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,19 +1,8 @@
 public class HelloWorld
 
 {
-   public static void main(String[] args)
-   {
-      System.out.print("Hello World!");
-   
-     //name is a reference variable
-      String name = "Goerge Washington";
-   
-      System.out.println(name); 
-   
-     //age is a primitive variable 
-      int age = 24;
-      
-      System.out.println(age);
-   
-   } //end main
-}//end class
+    public static void main(String[] args)
+    {
+       System.out.print("Hello World!");
+    }
+}       
