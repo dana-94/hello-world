b0343af647c6f9b79cde6030b91110d8f4ecdb24
dana <dalsiba0@email.cpcc.edu>
2021-02-09 11:54:21 -0500

    ubdate HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,19 @@
 public class HelloWorld
 
 {
-    public static void main(String[] args)
-    {
-       System.out.print("Hello World!");
-    }
-}       
+   public static void main(String[] args)
+   {
+      System.out.print("Hello World!");
+   
+     //name is a reference variable
+      String name = "Goerge Washington";
+   
+      System.out.println(name); 
+   
+     //age is a primitive variable 
+      int age = 24;
+      
+      System.out.println(age);
+   
+   } //end main
+}//end class
